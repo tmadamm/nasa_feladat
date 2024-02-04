2bec1963f7493e914ad4b9cf31639d17210874c4
tmadamm <tmadam@outlook.hu>
2024-02-04 20:18:43 +0100

    'kész feladat'

---
+++ nasa_feladat/nasa.py
@@ -0,0 +1,34 @@
+def beolvas():
+    elofordulasok = [0]*12
+    with open('astronauts.csv') as bemenet:
+        for sor in bemenet.readlines()[1:]:
+            sor = sor.strip().split(",")
+            elofordulasok[int(sor[4].split("/")[0])-1] += 1
+    return elofordulasok
+
+
+def leggyakoribb_ho():
+    eloford = beolvas()
+    osszeg = sum(eloford)
+    return [find_max(eloford, osszeg) for _ in range(3)]
+
+
+def find_max(elofordulasok, osszeg):
+    max_honap = 0
+    jelenlegi_max = 0
+    for i in range(len(elofordulasok)):
+        if elofordulasok[i] > jelenlegi_max:
+            jelenlegi_max = elofordulasok[i]
+            max_honap = i
+    elofordulasok[max_honap] = 0
+    return {max_honap+1: (jelenlegi_max/osszeg)*100}
+
+
+def main():
+    leggyakoribbak = leggyakoribb_ho()
+    for ertekek in leggyakoribbak:
+        for honap in ertekek:
+            print(f"{honap}. hónap: {ertekek[honap]:.1f}%")
+
+
+main()
